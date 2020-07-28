83de827902d795c959d9b26911a445547dd8ac19
Devilzac <shane199274@gmail.com>
2020-07-28 21:03:45 +0200

    4.1 terminado

--- GreenSock/4.1 - Estructura/inicio/app.js
+++ GreenSock/4.1 - Estructura/inicio/app.js
@@ -6,14 +6,23 @@ var btnPausar;
 var btnReversa;
 var btnPlay;
 
-window.onload = function() {
+window.onload = function () {
     btnAnimar = document.getElementById("btnAnimar");
     btnReiniciar = document.getElementById("btnReiniciar");
     logo = document.getElementById("logo");
     btnAnimar.onclick = animar;
+    btnReiniciar.onclick = reiniciar;
 }
 
 function animar() {
-
+    animacion = TweenLite.to(logo, 2, {
+        left: "100%",
+        backgroundColor: "#000",
+        borderRadius: 50,
+        ease: Elastic.easeIn
+    });
 }
 
+function reiniciar() {
+    animacion.restart();
+}

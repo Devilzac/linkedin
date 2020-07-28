093eb70678e967603788ac63ba0639d7123c39a2
Devilzac <shane199274@gmail.com>
2020-07-28 21:38:07 +0200

    4.3 finished

--- GreenSock/4.3 - TweenMax/inicio/app.js
+++ GreenSock/4.3 - TweenMax/inicio/app.js
@@ -4,7 +4,7 @@ var btnReiniciar;
 var btnAnimar;
 var btnAnimarImg;
 
-window.onload = function() {
+window.onload = function () {
     btnAnimar = document.getElementById("btnAnimar");
     btnReiniciar = document.getElementById("btnReiniciar");
     btnAnimarImg = document.getElementById("btnAnimarImg");
@@ -18,17 +18,32 @@ function animar() {
     animacion = TweenMax.to(logo, 2, {
         left: 400,
         backgroundColor: "black",
-        borderRadius: 50
+        borderRadius: 50,
+        ease: Back.easeInOut,
+        repeat: 2,
+        repeatDelay: 0,
+        yoyo: true
     });
 
 
-    btnReiniciar.onclick = function() {
+    btnReiniciar.onclick = function () {
         animacion.restart();
     }
 }
 
-function animarImg(){
+function animarImg() {
     var img1 = document.getElementById("img1");
     var img2 = document.getElementById("img2");
     var img3 = document.getElementById("img3");
+
+    TweenMax.staggerTo([img1, img2, img3], 2, {
+            left: 400,
+            backgroundColor: "black",
+            borderRadius: 50,
+            ease: Back.easeInOut,
+            repeat: 2,
+            repeatDelay: 0,
+            yoyo: true
+        },
+        0.5);
 }
